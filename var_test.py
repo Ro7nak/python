47c9ac0e0ada9a5a567bf5ed906f31ef87af5306
Ro7nak <rounak077@gmail.com>
2018-09-06 23:50:50 +0530

    adding startng sample of codes

---
+++ var_test.py
@@ -0,0 +1,22 @@
+print("there was once a man named mike, ")
+print("he was 70 years old. ")
+print("he really liked the name mike, ")
+print("but didn't lke being 70.")
+print("")
+user_name = 'john'
+age = '35'
+
+
+print("there was once a man named " + user_name + ", ")
+print("he was " + age + " years old. ")
+print("he really liked the name " + user_name + ", ")
+print("but didn't lke being " + age + ".")
+print("")
+
+user_name = 'jack'
+age = '50'
+
+print("there was once a man named " + user_name + ", ")
+print("he was " + age + " years old. ")
+print("he really liked the name " + user_name + ", ")
+print("but didn't lke being " + age + ".")
