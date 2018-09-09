ce942d88fcc98484cbb4b8a713e5fa02db95d9a0
Ro7nak <rounak077@gmail.com>
2018-09-09 18:03:30 +0530

    basic tuples and return statement
    adding samples done on tuples and return

---
+++ return_statement_sample.py
@@ -0,0 +1,9 @@
+
+def cube(a):
+    return a*a*a  # cant enter anything after return statement
+
+
+num = input("enter number to get cube for: ")
+result = cube(int(num))
+print(result)
+
---
+++ tuples_sample.py
@@ -0,0 +1,8 @@
+
+coordinates = (4, 5)
+# immutable, can not change once created
+
+print(coordinates[0])
+
+coordinates2 = [(4, 5), (6, 7)]
+# list of tuples
