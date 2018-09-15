674e2ed677f5763ba2c8db360d61c64d0307b26d
Ro7nak <rounak077@gmail.com>
2018-09-15 14:45:18 +0530

    use of classes in python
    sample use of classes in python

---
+++ Classes_and_object/app.py
@@ -0,0 +1,5 @@
+from Classes_and_object.Student import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+
+print(student1.name)
---
+++ app_use_classStudent.py
@@ -0,0 +1,6 @@
+from Student import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+
+student2 = Student("kenny", "Art", 3.3, True)
+print(student1.name)
