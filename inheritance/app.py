e95df0f3ce3f18d261bd28a198014c943ad6fc9a
Ro7nak <rounak077@gmail.com>
2018-09-15 20:52:41 +0530

    inheritance
    simple examples with use of inheritance in python

---
+++ inheritance/app.py
@@ -0,0 +1,9 @@
+from inheritance.Chef import Chef
+from inheritance.chineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_chicken()
+myChef.make_special_dish()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_special_dish()
