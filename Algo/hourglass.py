5ce525d76bdddd6141249c960ab2da29be5ca1e9
Ro7nak <rounak077@gmail.com>
2018-10-04 22:03:42 +0530

    ALGO DONE IN OFFICE

--- Algo/hourglass.py
+++ Algo/hourglass.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 k = int(input("enter number: "))
 for i in range(k):  # for upper
     for j in range(i):
@@ -11,3 +12,18 @@ for a in range(k-1):  # for lower
     for c in range(a+2):
         print("* ", end="")
     print("\r")
+=======
+k = int(input("enter number: "))
+for i in range(k):  # for upper
+    for j in range(i):
+        print(" ", end="")
+    for z in range(k-i):
+        print("* ", end="")
+    print("\r")
+for a in range(k-1):  # for lower
+    for b in range(k-2-a):
+        print(" ", end="")
+    for c in range(a+2):
+        print("* ", end="")
+    print("\r")
+>>>>>>> 8287f82b3a8c4d2357374234cd6f52e4813e2603
