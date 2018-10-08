3f31ddb8c121765e9b2398b0138489dda1cbd6b8
Ro7nak <rounak077@gmail.com>
2018-10-08 18:31:08 +0530

    jumping on clouds example from hacker rank

---
+++ Collections/orderedDict.py
@@ -0,0 +1,25 @@
+# https://www.hackerrank.com/challenges/py-collections-ordereddict/problem
+
+
+from collections import OrderedDict
+d = OrderedDict()
+for _ in range(int(input())):
+    item, space, quantity = input().rpartition(' ')
+    d[item] = d.get(item, 0) + int(quantity)
+for item, quantity in d.items():
+    print(item, quantity)
+
+
+'''
+sample input :
+9
+BANANA FRIES 12
+POTATO CHIPS 30
+APPLE JUICE 10
+CANDY 5
+APPLE JUICE 10
+CANDY 5
+CANDY 5
+CANDY 5
+POTATO CHIPS 30
+'''
